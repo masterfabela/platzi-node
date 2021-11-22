32070b3542ac88f931f3c58802dbd23ec75d3a40
Jose Ramon Vieites <jose.vieites@altia.es>
2021-11-22 21:00:33 +0100

    Clase 7

---
+++ async/callback.js
@@ -0,0 +1,25 @@
+function hola(nombre, callback) {
+  setTimeout(() => {
+    console.log('Hola, ' + nombre);
+    callback();
+  }, 1000);
+}
+
+function adios(nombre, callback) {
+  setTimeout(() => {
+    console.log('Adios ' + nombre);
+    callback();
+  }, 500);
+}
+
+/*
+console.log('Iniciando proceso');
+hola('Jose', () => {
+  adios('Jose', () => {
+    console.log('Terminando proceso');
+  });
+});
+*/
+
+hola('Carlos', () => {});
+adios('Carlos', () => {});
